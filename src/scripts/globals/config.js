5b953a415d4b10ec8470c9c47f73316ed5de525e
rulhuda <nurulhuda.unhasy@gmail.com>
2022-05-01 07:53:26 +0700

    Modified CACHE_NAME in globals/config.js

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   BASE_IMAGE_URL_S: 'https://restaurant-api.dicoding.dev/images/small/',
   BASE_IMAGE_URL_M: 'https://restaurant-api.dicoding.dev/images/medium/',
   BASE_IMAGE_URL_L: 'https://restaurant-api.dicoding.dev/images/large/',
-  CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 'SerestaCache',
   DATABASE_NAME: 'SerestaRestaurant',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
