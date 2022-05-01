2874cb87a75cbf05081032b56fbcc64a6ce5dd99
rulhuda <nurulhuda.unhasy@gmail.com>
2022-05-01 07:54:59 +0700

    modified aria-label in like and liked Button Template

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -72,13 +72,13 @@ const createRestaurantItemTemplate = (restaurant) => `
 `;
 
 const createLikeButtonTemplate = () => `
-  <button aria-label="like this movie" id="likeButton" class="like">
+  <button aria-label="like this restaruant" id="likeButton" class="like">
     <i class="fa fa-heart-o" aria-hidden="true"></i>
   </button>
 `;
 
 const createLikedButtonTemplate = () => `
-  <button aria-label="unlike this movie" id="likeButton" class="liked">
+  <button aria-label="unlike this restaurant" id="likeButton" class="liked">
     <i class="fa fa-heart" aria-hidden="true"></i>
   </button>
 `;
